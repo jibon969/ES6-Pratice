2c4e57cbfe66ce4ddc4b5820afe92b26fd82c377
jayed969 <jayed.swe@gmail.com>
2023-01-23 12:01:51 +0600

    added Template string, multiple line string

---
+++ 03-template-string.js
@@ -0,0 +1,20 @@
+
+const firstName = "jibon";
+const lastName = "ahmed";
+
+const fullName = firstName + lastName
+console.log(fullName)
+
+// template string 
+const fullName2 = `${firstName} ${lastName}`
+console.log(fullName2)
+
+
+// Multiline String
+const multiline = `
+I live you
+I will you
+no. i don't need you
+Baily road e dorker nai
+`
+console.log(multiline)
