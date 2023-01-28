11c655e190633972875f92211b58b84bc482dc59
jayed969 <jayed.swe@gmail.com>
2023-01-28 21:11:08 +0600

    added double == vs triple ===

---
+++ 08-double-vs-triple-equal.js
@@ -0,0 +1,22 @@
+
+// Double equal (==) vs triple (===)
+
+const first = 0;
+const second = false;
+if(first==second){
+    console.log("Condation False")
+}
+else{
+    console.log("Condation true")
+}
+
+
+// triple (===) type 
+const a = 0;
+const b = false;
+if(a === b){
+    console.log("Condation False")
+}
+else{
+    console.log("Condation true")
+}
