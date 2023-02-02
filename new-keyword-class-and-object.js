563c6dfbb80048609208d9dfc56b30ea1b1310ab
jayed969 <jayed.swe@gmail.com>
2023-02-02 11:46:07 +0600

    added new keyword class and object

---
+++ new-keyword-class-and-object.js
@@ -0,0 +1,11 @@
+
+class Person{
+    constructor(firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+
+const heroPerson = new Person("Jibon", "Ahmed", 600)
+console.log(heroPerson)
