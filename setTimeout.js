7aba90bed589d3e100f17c51323820b71ea12270
jayed969 <jayed.swe@gmail.com>
2023-02-03 23:00:27 +0600

    added setTimeout & setInterval

--- setTimeout.js
+++ setTimeout.js
@@ -6,6 +6,13 @@ function doSomeThing(name){
 const result = doSomeThing("Jibon");
 console.log(result)
 
+// run just one time
 setTimeout(()=>{
     console.log("See you later !")
 }, 10000)
+
+
+// repeat 1000 later 
+setInterval(()=>{
+    console.log("Do it")
+}, 1000)
