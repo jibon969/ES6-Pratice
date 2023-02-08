718309686354f48f8da521b61e4c76de66b91eb2
jayed969 <jayed.swe@gmail.com>
2023-02-08 14:04:34 +0600

    added localStorage set and read value

---
+++ localStorage/localStorage.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-add-name").addEventListener('click', function(){
+    const nameField = document.getElementById("name-field")
+    const nameValue = nameField.value;
+    console.log(nameValue);
+    localStorage.setItem("name", nameValue);
+})
+
+document.getElementById("btn-remove-name").addEventListener("click", function(){
+    localStorage.removeItem("name");
+})
+
+document.getElementById("btn-clear-ls").addEventListener("click", function(){
+    localStorage.clear()
+})
