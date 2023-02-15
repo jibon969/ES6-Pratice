b9a12a85a98a3e8e521c09062d7ecb54e4fe9e16
jayed969 <jayed.swe@gmail.com>
2023-02-15 23:21:08 +0600

    added some example primited & non-primitive data

--- primitive-non-primitive.js
+++ primitive-non-primitive.js
@@ -11,3 +11,16 @@ console.log(typeof a, typeof b, typeof c)
 const d = [10, 14, 15, 20]
 const e = {id:1, class:9}
 console.log(typeof d, typeof e)
+
+// More Example
+let p = {job:"Software Engineer"}
+let q = p;
+q = {job:"Backend Engineer"}
+console.log(p, q)
+
+
+// More Example
+let x = {job:"Software Engineer"}
+let y = x;
+q.job = "Backend Developer"
+console.log(x, y)
