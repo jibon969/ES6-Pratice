580a584c8c0b1e02b7d9443f5e025f01891255b8
jayed969 <jayed.swe@gmail.com>
2023-02-16 22:50:15 +0600

    added some example

---
+++ 20-truthy-and-falsy-value.js
@@ -0,0 +1,19 @@
+
+const x = 4;
+if(x){
+    console.log("value of x is truthy")
+}
+else{
+    console.log("value of x is false")
+}
+
+// Example optional check
+const y = false
+if(!y){
+    console.log("value of y is truthy", y)
+}
+else{
+    console.log("value of y is false", y)
+}
+
+// More Example
