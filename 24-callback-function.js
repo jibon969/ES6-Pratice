1896fc5f88b9fbfed26575e3acfa407622ba7d98
jayed969 <jayed.swe@gmail.com>
2023-02-17 17:40:11 +0600

    added callback function

---
+++ 24-callback-function.js
@@ -0,0 +1,20 @@
+
+// Example 1 : Pass different vale
+function example (name){
+    console.log(name)
+}
+
+const result1 = "Jibon Ahmed";
+console.log(result1)
+
+const result2 = [10, 20, 30, 40]
+console.log(result2)
+
+const result3 = {id:1, name:"Payel", dept:"SWE"}
+console.log(result3)
+
+const result4 = true
+console.log(result4)
+
+const result5 = false
+console.log(result5)
