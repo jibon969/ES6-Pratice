7e695000c2251212228086f875a46ce14aa85651
jayed969 <jayed.swe@gmail.com>
2023-03-02 14:56:04 +0600

    added async function

---
+++ async-await-explained/async-await-explained.js
@@ -0,0 +1,14 @@
+// async await explained
+async function hello(){
+    return "Hello"
+}
+
+console.log(hello())
+
+
+// Normal Function
+function hello(){
+    return "Hello"
+}
+
+console.log(hello())
