b4056bf7dc4c4c8180dec3616728370ab28b355e
jayed969 <jayed.swe@gmail.com>
2023-03-22 23:50:45 +0600

    added some link

--- Explore-Browser-Debug/03-javascript-engine-vs-v8-internal-mechanism.js
+++ Explore-Browser-Debug/03-javascript-engine-vs-v8-internal-mechanism.js
@@ -1,2 +1,10 @@
 
 // Javascript engine-vs-v8-internal-mechanism
+
+/*
+    https://cabulous.medium.com/how-v8-javascript-engine-works-5393832d80a7
+
+    https://www.stackpath.com/edge-academy/what-is-v8-javascript-engine/
+
+*/
+
