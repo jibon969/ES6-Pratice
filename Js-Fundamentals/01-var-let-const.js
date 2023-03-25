afb7099d6e570e0ecd89eafa29f68a03f999229d
jayed969 <jayed.swe@gmail.com>
2023-03-25 23:13:58 +0600

    added js fundamentals

---
+++ Js-Fundamentals/01-var-let-const.js
@@ -0,0 +1,22 @@
+
+// var let const
+
+// Example of var
+var a = 5;
+console.log("Before :",a)
+a = 6;
+console.log("After : ",a)
+
+
+// Example of let
+let b = 5;
+console.log("Before :",b)
+b = 6;
+console.log("After : ",b)
+
+
+// Example of const
+const c = 5;
+console.log("Before :",c)
+// c = 6;
+// console.log("After : ",c)
