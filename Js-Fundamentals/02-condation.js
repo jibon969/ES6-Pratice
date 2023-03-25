20294e6210647839250a776effb6ed161b6177b6
jayed969 <jayed.swe@gmail.com>
2023-03-25 23:19:56 +0600

    added condation

---
+++ Js-Fundamentals/02-condation.js
@@ -0,0 +1,14 @@
+const fatherName = "Shakib";
+let season = "Raniy";
+season = "winter";
+
+
+if(fatherName==="Shakib" || season==="winter"){
+    console.log(true)
+}
+else if(fatherName==="Shakib"){
+    console.log(true)
+}
+else{
+    console.log("Nop")
+}
